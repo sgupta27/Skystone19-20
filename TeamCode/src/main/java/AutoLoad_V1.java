22cb0ba5fc62ba773c43061845f7725987d52cda
sahithi-thumuluri <sahithi.thumuluri@gmail.com>
2019-11-03 16:46:43 -0500

    outlines of both blue auto programs

--- TeamCode/src/main/java/AutoLoad_V1.java
+++ TeamCode/src/main/java/AutoLoad_V1.java
@@ -4,11 +4,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name = "AutoLoad_V1")
 public class AutoLoad_V1 extends LinearOpMode
 {
-    private MecBot mecBot;
+    private MecBot holo;
 
     public void runOpMode()
     {
-        mecBot = new MecBot(hardwareMap, this);
+        holo = new MecBot(hardwareMap, this);
         waitForStart();
+
+        //blue loading side, robot facing the cubes in the middle, outside of the left wheels lined
+        //up with the outside edge closer to the build zone of the second square from the audience side
+        holo.driveStraight_Inches(27, .8);
+        //check for color of cubes and get correct cube
+        holo.driveStraight_Inches(-5, .8);
+        holo.driveStrafe_Inches(-52, .8);
+        //drop off cube
+        holo.driveStrafe_Inches(16, .8);
     }
 }

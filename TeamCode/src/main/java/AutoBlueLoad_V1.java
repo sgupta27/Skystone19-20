1cc02f221929dc1562d1e160b7ef8771ece07668
Team machine <ftc6150@gmail.com>
2019-11-17 14:43:30 -0500

    fixed telemetry and color errors

--- TeamCode/src/main/java/AutoBlueLoad_V1.java
+++ TeamCode/src/main/java/AutoBlueLoad_V1.java
@@ -9,18 +9,16 @@ public class AutoBlueLoad_V1 extends LinearOpMode
 
     public void runOpMode()
     {
-        holo = new MecBot(hardwareMap, this);
+        holo = new MecBot(hardwareMap,this);
         waitForStart();
 
         //blue loading side, robot facing the cubes in the middle, outside of the left wheels lined
         //up with the outside edge closer to the build zone of the second square from the audience side
         holo.driveStraight_Inches(25, .8);
-        telemetry.addData("starting looking for color", null);
-        telemetry.update();
-        sleep(3000);
-        char colorSkystone = holo.getColor(holo.getFrontColorSens);
+        char colorSkystone = holo.getColor();
         telemetry.addData("stone color = ", colorSkystone);
         telemetry.update();
+        
         //check for color of cubes and get correct cube
         /*holo.driveStraight_Inches(-5, .8);
         holo.driveStrafe_Inches(-52, .8);

708e0044b9a41bfa4cc901052fe0983b1ce5cb2b
sgupta27 <sgupta27@ymail.com>
2019-11-29 18:44:17 -0500

    Fixed error with getting lights

--- TeamCode/src/main/java/MecTeleController.java
+++ TeamCode/src/main/java/MecTeleController.java
@@ -11,8 +11,8 @@ public class MecTeleController extends OpMode
 
 
 
-    public void init()
     {
+    public void init()
         holo = new MecBot(hardwareMap, this);
         gamepad1.setJoystickDeadzone(.25f);
         gamepad2.setJoystickDeadzone(.25f);
